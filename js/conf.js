1780159e86aa9d94ea8e1fe1558576407ae9da8a
arcadio <arcadio@debi>
2016-04-22 19:54:54 +0200

    Addition of the GMAP library and first example of usage

---
+++ js/conf.js
@@ -0,0 +1,46 @@
+(function (CONF, undefined) {
+
+    CONF.map = {
+        mapCfg: {
+            scrollwheel: false,
+            center: new google.maps.LatLng(37.794850850976495, -24.65378750000002),
+            zoom: 3,
+            panControl: true,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL
+            },
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+            },
+            scaleControl: true,
+            streetViewControl: true,
+            overviewMapControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        },
+        infoBoxCfg: {
+            disableAutoPan: false,
+            maxWidth: '150px',
+            pixelOffset: new google.maps.Size(-90, -55),
+            alignBottom: true,
+            zIndex: null,
+            boxStyle: {
+                //background: "url('http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/examples/tipbox.gif') no-repeat",
+                //opacity: 0.9,
+                width: '300px'
+            },
+            closeBoxMargin: '12px 4px 2px 2px',
+            closeBoxURL: 'http://www.google.com/intl/en_us/mapfiles/close.gif',
+            infoBoxClearance: new google.maps.Size(1, 1)
+        },
+        destinationLinesCfg: {
+            strokeColor: "#039",
+            strokeOpacity: 0.5,
+            geodesic: true,
+            strokeWeight: 2
+        }
+    };
+
+})(window.CONF = window.CONF || {});
+
---
+++ js/gMap.js
@@ -0,0 +1,100 @@
+/* global google, _ */
+(function (G_MAP, $, _, undefined) {
+    "use strict";
+
+    /* Private bars */
+    var mapCfg,
+        infoBox, 
+        map, 
+        markers = [],
+        markerEvents = {
+            click: 'G_MAP:markerClicked'
+        };
+
+    /* Private functions */
+    function addEventsToMarker(marker) {
+        google.maps.event.addListener(marker, 'click', function () {
+            mapCfg.$eventListener.trigger(markerEvents.click, [marker]);
+        });
+    }
+    
+    function initMarkers(arrObjs) {
+        infoBox = new InfoBox(mapCfg.infoBoxCfg);
+        markers = _.reduce(arrObjs, function (memo, obj) {
+            var marker = new google.maps.Marker({
+                position: obj.position,
+                map: map,
+                infoBoxHtml: obj.infoBoxHtml,
+                all: obj
+            });
+            addEventsToMarker(marker);
+            return memo.concat(marker);
+        }, []);
+    }
+
+    function closeInfoBox() {
+        if (infoBox) {
+            infoBox.close();
+        }
+    }
+
+    function resetAllMarkers() {
+        closeInfoBox();
+        markers = _.reduce(markers, function(_, marker) {
+            marker.setMap(null);
+        }, []);
+    }
+    
+    function getMap() {
+        return map ? map : createMap();
+    }
+    
+    function createMap() {
+        map = new google.maps.Map(mapCfg.canvasEl, mapCfg.mapCfg);
+    }
+
+    /* public interface */
+    G_MAP.openMarkerInfo = function(marker) {
+        infoBox.setContent(marker.infoBoxHtml);
+        infoBox.open(map, marker);
+    };
+    
+    G_MAP.getMarkerEvent = function (str) {
+        return markerEvents[str];
+    };
+
+    // google.maps.LatLng
+    G_MAP.setDestinationLines = function (from) {
+        _.each(markers, function (marker) {
+            var to = marker.position;
+            if (!from.equals(to)) {
+                new google.maps.Polyline(_.extend({}, mapCfg.destinationLinesCfg, {map: map, path: [from, to]}));
+            }
+        });
+    };
+
+    // { lat: double, lon: double, title: string, extra: obj, icon: string, shadowIcon: string }
+    G_MAP.createMarkersFromArray = function (arr) {
+        resetAllMarkers();
+        initMarkers(arr);
+        return this;
+    };
+
+    G_MAP.setZoom = function (newZoom) {
+        getMap().setZoom(newZoom);
+        return this;
+    };
+
+    // google.maps.LatLng
+    G_MAP.setCenterCoords = function (mapsLatLng) {
+        getMap().setCenter(mapsLatLng);
+        return this;
+    };
+    
+    // { canvasEl: htmlObj, $eventListener: jQueryObj, mapCfg: obj, infoBoxCfg: obj, destinationLinesCfg: obj}
+    G_MAP.initMap = function (obj) {
+        mapCfg = obj;
+        createMap();
+        return this;
+    };
+}(window.G_MAP = window.G_MAP || {}, jQuery, _));
